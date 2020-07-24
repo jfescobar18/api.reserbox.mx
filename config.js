0929982fbbdbc070c6c0c9ed4702f88cc42c19e5
Francisco Escobar <jfescobar18@hotmail.com>
2020-07-23 20:10:24 -0500

    Project start up

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {};
+
+config.console = {};
+
+config.console.greenColor = "\x1b[32m%s\x1b[0m";
+config.console.yellowColor = "\x1b[33m%s\x1b[0m";    
+config.console.RedColor = "\x1b[31m%s\x1b[0m";
+
+module.exports = config;
